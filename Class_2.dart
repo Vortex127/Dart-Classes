361f0fd753b86b94827af3b63cff17a31e2d1f4c
Vortex127 <mirza.business15@gmail.com>
2023-07-28 16:46:15 +0500

    add class2.dart and class3.dart

---
+++ Class_2.dart
@@ -0,0 +1,19 @@
+void main() {
+  bool abc = 12 < 8;
+  print(abc);
+
+  int n1 = 5;
+  int n2 = 2;
+  num r = n1 ~/ n2; //the "~" operator rounds off
+  print(r);
+
+  String email = "mirzasfandjhjh";
+  String password = "Stragate";
+  bool cond = email == "mirzasfandjhjh" && password == "Stragate";
+  print(cond);
+
+  int age = 18;
+  print(age == 18);
+}
+
+// studied about if else, operators, and other such stuff
